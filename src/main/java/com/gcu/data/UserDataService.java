16220d52262efb5df6da669c4fd13c0c912db005
Payson Parker <payson.parker@comcast.net>
2022-10-26 12:43:27 -0700

    Create UserDataService.java
    Created UserDataService class that implements the DataAccessInterface.

---
+++ src/main/java/com/gcu/data/UserDataService.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.gcu.data;
+
+import java.util.List;
+
+import com.gcu.model.UserModel;
+
+/**
+ * @author paysonparker
+ *
+ */
+public class UserDataService implements DataAccessInterface<UserModel> {
+
+	@Override
+	public List<UserModel> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserModel findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserModel findByUsername(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean create(UserModel userModel) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(UserModel userModel) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(UserModel userModel) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
