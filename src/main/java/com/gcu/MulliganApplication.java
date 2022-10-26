26becbde5b67afe2def9534afcddbb9f4bbdbd57
Payson Parker <payson.parker@comcast.net>
2022-10-26 12:37:24 -0700

    Component Scan annotation
    Added the component scan annotation for the group ID of this application.

--- src/main/java/com/gcu/MulliganApplication.java
+++ src/main/java/com/gcu/MulliganApplication.java
@@ -2,8 +2,10 @@ package com.gcu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.gcu"})
 public class MulliganApplication {
 
 	public static void main(String[] args) {
