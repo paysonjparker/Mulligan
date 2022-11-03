2f701c9ad5db83cc915bfaf9d8cfb1eaf5d59e5f
Payson Parker <payson.parker@comcast.net>
2022-11-02 20:42:40 -0700

    Added methods
    display, makePost, edit, and delete methods

--- src/main/java/com/gcu/controller/PostController.java
+++ src/main/java/com/gcu/controller/PostController.java
@@ -3,19 +3,48 @@
  */
 package com.gcu.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.gcu.model.PostModel;
 
 /**
  * @author paysonparker
  *
  */
 @Controller
+@RequestMapping("/post")
 public class PostController {
 
-	@GetMapping("/post")
+
+	
+	@GetMapping("/")
 	public String display(Model model) {
 		return "post";
+	}
+	
+	@PostMapping("/makePost")
+	public String makePost(@Valid PostModel postModel, BindingResult bindingResult, Model model)
+	{
+			return "index";
+	}
+	
+	@GetMapping("/edit")
+	public String edit(Model model, @RequestParam(name="id", required = true) Integer postId) 
+	{
+		return "editPost";
+	}
+	
+	@GetMapping("/delete")
+	public String delete(Model model, @RequestParam(name="id", required = true) Integer postId) 
+	{
+		return "index";
 	}
 }
