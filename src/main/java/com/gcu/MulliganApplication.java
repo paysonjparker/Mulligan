9e872635499825da6a5ca4fd84ce985f416bb8a6
Payson Parker <payson.parker@comcast.net>
2022-11-02 19:42:42 -0700

    Removed excluding dataSource auto config

--- src/main/java/com/gcu/MulliganApplication.java
+++ src/main/java/com/gcu/MulliganApplication.java
@@ -2,10 +2,9 @@ package com.gcu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @ComponentScan({"com.gcu"})
 public class MulliganApplication {
 
