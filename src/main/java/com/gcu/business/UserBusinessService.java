4997b2f9add80a8d9e7774255653e039f07262d4
Payson Parker <payson.parker@comcast.net>
2022-11-05 12:51:18 -0700

    Added service variable

--- src/main/java/com/gcu/business/UserBusinessService.java
+++ src/main/java/com/gcu/business/UserBusinessService.java
@@ -3,11 +3,21 @@
  */
 package com.gcu.business;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gcu.data.DataAccessInterface;
+import com.gcu.model.UserModel;
 
 /**
  * @author paysonparker
  *
  */
+@Service
 public class UserBusinessService {
 
+	@Autowired
+	private DataAccessInterface<UserModel> service;
+	
+	
 }
