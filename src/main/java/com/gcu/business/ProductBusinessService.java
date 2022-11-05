cd32422febf52bd38281ec90833d7d74c676a8d0
Payson Parker <payson.parker@comcast.net>
2022-11-05 12:51:01 -0700

    Added service and methods

--- src/main/java/com/gcu/business/ProductBusinessService.java
+++ src/main/java/com/gcu/business/ProductBusinessService.java
@@ -3,10 +3,67 @@
  */
 package com.gcu.business;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gcu.data.DataAccessInterface;
+import com.gcu.model.PostModel;
+
 /**
  * @author paysonparker
  *
  */
+@Service
 public class ProductBusinessService {
 
+	@Autowired
+	DataAccessInterface<PostModel> service;
+	
+	/**
+	 * Returns all posts from database
+	 * @return list of post
+	 */
+	public List<PostModel> getPosts()
+	{
+		return service.findAll();
+	}
+	
+	/**
+	 * Gets a post by its ID number.
+	 * @param id Id being searched for.
+	 * @return The post with that ID number if it exists.
+	 */
+	public PostModel getPostById(int id)
+	{
+		return service.findById(id);
+	}
+	
+	/**
+	 * Adds a post to the database.
+	 * @param post Post model being created.
+	 */
+	public void addPost(PostModel post)
+	{
+		service.create(post);
+	}
+
+	/**
+	 * Updates a post's variables.
+	 * @param post Post being edited.
+	 */
+	public void update(PostModel post) 
+	{
+		service.update(post);
+	}
+
+	/**
+	 * Deletes a post from the database.
+	 * @param post Post model being deleted.
+	 */
+	public void delete(PostModel post) 
+	{
+		service.delete(post);
+	}
 }
