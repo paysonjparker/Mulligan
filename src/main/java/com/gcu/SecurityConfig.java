1f94e04a89f3bb6858a149dcd61a19e66b2b0fc7
Payson Parker <payson.parker@comcast.net>
2022-11-15 18:18:59 -0700

    Removed web security annotations

--- src/main/java/com/gcu/SecurityConfig.java
+++ src/main/java/com/gcu/SecurityConfig.java
@@ -4,14 +4,12 @@
 package com.gcu;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * @author paysonparker
  *
  */
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
 	
