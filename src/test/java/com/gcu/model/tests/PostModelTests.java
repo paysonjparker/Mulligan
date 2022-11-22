adbd1e1c7c3223c79e84743526f4499b2b8d9bc9
Payson Parker <payson.parker@comcast.net>
2022-11-22 08:27:59 -0800

    Added JUnit test case files for the model classes

---
+++ src/test/java/com/gcu/model/tests/PostModelTests.java
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+package com.gcu.model.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.gcu.model.PostModel;
+
+/**
+ * @author paysonparker
+ *
+ */
+class PostModelTests {
+
+	/**
+	 * Test method for {@link com.gcu.model.PostModel#PostModel(int, java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	void testPostModelIntStringStringString() {
+		PostModel testPost = new PostModel();
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.PostModel#getPostId()}.
+	 */
+	@Test
+	void testGetPostId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.PostModel#setPostId(int)}.
+	 */
+	@Test
+	void testSetPostId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.PostModel#getContent()}.
+	 */
+	@Test
+	void testGetContent() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.PostModel#setContent(java.lang.String)}.
+	 */
+	@Test
+	void testSetContent() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.PostModel#getPostTime()}.
+	 */
+	@Test
+	void testGetPostTime() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.PostModel#setPostTime(java.lang.String)}.
+	 */
+	@Test
+	void testSetPostTime() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.PostModel#getAuthor()}.
+	 */
+	@Test
+	void testGetAuthor() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.PostModel#setAuthor(java.lang.String)}.
+	 */
+	@Test
+	void testSetAuthor() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/gcu/model/tests/UserModelTests.java
@@ -0,0 +1,136 @@
+/**
+ * 
+ */
+package com.gcu.model.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author paysonparker
+ *
+ */
+class UserModelTests {
+
+	/**
+	 * Test method for {@link com.gcu.model.UserModel#UserModel(int, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, double)}.
+	 */
+	@Test
+	void testUserModelIntStringStringStringStringStringDouble() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.UserModel#getUserId()}.
+	 */
+	@Test
+	void testGetUserId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.UserModel#setUserId(int)}.
+	 */
+	@Test
+	void testSetUserId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.UserModel#getUsername()}.
+	 */
+	@Test
+	void testGetUsername() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.UserModel#setUsername(java.lang.String)}.
+	 */
+	@Test
+	void testSetUsername() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.UserModel#getPassword()}.
+	 */
+	@Test
+	void testGetPassword() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.UserModel#setPassword(java.lang.String)}.
+	 */
+	@Test
+	void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.UserModel#getName()}.
+	 */
+	@Test
+	void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.UserModel#setName(java.lang.String)}.
+	 */
+	@Test
+	void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.UserModel#getEmail()}.
+	 */
+	@Test
+	void testGetEmail() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.UserModel#setEmail(java.lang.String)}.
+	 */
+	@Test
+	void testSetEmail() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.UserModel#getHomeCourse()}.
+	 */
+	@Test
+	void testGetHomeCourse() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.UserModel#setHomeCourse(java.lang.String)}.
+	 */
+	@Test
+	void testSetHomeCourse() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.UserModel#getHandicapIndex()}.
+	 */
+	@Test
+	void testGetHandicapIndex() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.model.UserModel#setHandicapIndex(double)}.
+	 */
+	@Test
+	void testSetHandicapIndex() {
+		fail("Not yet implemented");
+	}
+
+}
