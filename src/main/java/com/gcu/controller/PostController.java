327264e53eea6ecf5229eee701bbc3dcbbc08c5f
Payson Parker <payson.parker@comcast.net>
2022-11-21 17:09:30 -0800

    Added the makePost method

--- src/main/java/com/gcu/controller/PostController.java
+++ src/main/java/com/gcu/controller/PostController.java
@@ -3,8 +3,9 @@
  */
 package com.gcu.controller;
 
-import javax.validation.Valid; 
+import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.gcu.business.PostBusinessService;
 import com.gcu.model.PostModel;
 
 /**
@@ -23,28 +25,35 @@ import com.gcu.model.PostModel;
 @RequestMapping("/post")
 public class PostController {
 
-
+	@Autowired
+	PostBusinessService service;
 	
 	@GetMapping("/")
 	public String display(Model model) {
+		// display the register page.
+		model.addAttribute("postModel", new PostModel());
 		return "post";
 	}
-	
+
 	@PostMapping("/makePost")
-	public String makePost(@Valid PostModel postModel, BindingResult bindingResult, Model model)
-	{
-			return "index";
+	public String makePost(@Valid PostModel postModel, BindingResult bindingResult, Model model) {
+		// If post credentials are invalid, stay at post view
+		if (bindingResult.hasErrors()) {
+			return "register";
+		}
+
+		// If register credentials are valid, add post to db and return to main menu
+		service.addPost(postModel);
+		return "post";
 	}
-	
+
 	@GetMapping("/editPost")
-	public String editPost(Model model, @RequestParam(name="id", required = true) Integer postId) 
-	{
+	public String editPost(Model model, @RequestParam(name = "id", required = true) Integer postId) {
 		return "editPost";
 	}
-	
+
 	@GetMapping("/delete")
-	public String delete(Model model, @RequestParam(name="id", required = true) Integer postId) 
-	{
+	public String delete(Model model, @RequestParam(name = "id", required = true) Integer postId) {
 		return "index";
 	}
 }
