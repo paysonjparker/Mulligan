bb81c2742ef82a62d7dfe974474bf2942faa5a2c
Payson Parker <payson.parker@comcast.net>
2022-11-30 21:23:37 -0700

    added test case for businesses services

---
+++ src/test/java/com/gcu/business/tests/PostBusinessServiceTests.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.gcu.business.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author paysonparker
+ *
+ */
+class PostBusinessServiceTests {
+
+	/**
+	 * Test method for {@link com.gcu.business.PostBusinessService#getPosts()}.
+	 */
+	@Test
+	void testGetPosts() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.business.PostBusinessService#getPostById(int)}.
+	 */
+	@Test
+	void testGetPostById() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.business.PostBusinessService#addPost(com.gcu.model.PostModel)}.
+	 */
+	@Test
+	void testAddPost() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.business.PostBusinessService#update(com.gcu.model.PostModel)}.
+	 */
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.business.PostBusinessService#delete(com.gcu.model.PostModel)}.
+	 */
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/gcu/business/tests/UserBusinessServiceTests.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.gcu.business.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author paysonparker
+ *
+ */
+class UserBusinessServiceTests {
+
+	/**
+	 * Test method for {@link com.gcu.business.UserBusinessService#getUsers()}.
+	 */
+	@Test
+	void testGetUsers() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gcu.business.UserBusinessService#addUser(com.gcu.model.UserModel)}.
+	 */
+	@Test
+	void testAddUser() {
+		fail("Not yet implemented");
+	}
+
+}
