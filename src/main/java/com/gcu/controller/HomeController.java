09a2309c3c10122fd32f450f387b286240d14ca9
Payson Parker <payson.parker@comcast.net>
2022-12-02 06:58:43 -0700

    Added the doSearch method.
    Needs to be implemented

--- src/main/java/com/gcu/controller/HomeController.java
+++ src/main/java/com/gcu/controller/HomeController.java
@@ -3,12 +3,18 @@
  */
 package com.gcu.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.gcu.business.PostBusinessService;
+import com.gcu.model.PostModel;
+import com.gcu.model.UserModel;
 
 /**
  * @author paysonparker
@@ -25,6 +31,18 @@ public class HomeController {
 
 		model.addAttribute("posts", service.getPosts());
 		
+		return "index";
+	}
+	
+	@PostMapping("/doSearch")
+	public String doSearch(@Valid PostModel postModel, BindingResult bindingResult, Model model) {
+		// If post credentials are invalid, stay at post view
+		if (bindingResult.hasErrors()) {
+			return "post";
+		}
+
+		// If register credentials are valid, add post to db and return to main menu
+		model.addAttribute("posts", service.getPosts());
 		return "index";
 	}
 }
